a9ce7581959d3bb4bf0af6f528f3aef43eb913db
KimMinJun <manjun1004@naver.com>
2019-11-18 03:51:03 +0900

    Update HOGtest.cpp

--- HOG/HOGtest.cpp
+++ HOG/HOGtest.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main(int argc, char** argv){
-	Mat image;
-	String file
+int main() {
+	string img1 = "ZTK_video_2_Moment2.jpg";
+	Mat image = imread(img1);
+	imshow("test1",image);
+	waitKey(0);
+	return 0;
 }
